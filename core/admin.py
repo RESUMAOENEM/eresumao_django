41ce0dfd8cad46fd9d1bab0bde49013e3f4fa9bc
ERESUMAO <eresumao@gmail.com>
2022-10-31 19:49:26 -0300

    Customização do Admin

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,59 @@ from django.contrib import admin
 
 from core.models import Comentario, Curtida, Favorito, Resumo
 
-admin.site.register(Resumo)
-admin.site.register(Curtida)
-admin.site.register(Favorito)
-admin.site.register(Comentario)
+
+@admin.register(Resumo)
+class ResumoAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "conteudo", "data")
+    search_fields = ("titulo", "email")
+    list_filter = ("titulo",)
+    ordering = ("titulo", "data")
+
+
+@admin.register(Curtida)
+class CurtidaAdmin(admin.ModelAdmin):
+    list_display = (
+        "curtido_por",
+        "resumo",
+    )
+    search_fields = (
+        "curtido_por",
+        "resumo",
+    )
+    list_filter = (
+        "curtido_por",
+        "resumo",
+    )
+    ordering = (
+        "curtido_por",
+        "resumo",
+    )
+
+
+@admin.register(Favorito)
+class FavoritoAdmin(admin.ModelAdmin):
+    list_display = (
+        "favoritado_por",
+        "resumo",
+    )
+    search_fields = (
+        "favoritado_por",
+        "resumo",
+    )
+    list_filter = (
+        "favoritado_por",
+        "resumo",
+    )
+    ordering = (
+        "favoritado_por",
+        "resumo",
+    )
+
+
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ("comentado_por", "texto", "data")
+    search_fields = ("comentado_por", "data")
+    list_filter = ("comentado_por", "data")
+    ordering = ("comentado_por", "data")
+    list_per_page = 25
